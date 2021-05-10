3405afe433215989ade1dbdbb587e9eb282dd5ac
manuel <manuelbonini17@gmail.com>
2021-05-10 23:49:47 +0200

    header

---
+++ src/app.js
@@ -0,0 +1,18 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            liNav: [
+                'Home',
+                'Pages',
+                'Courses',
+                'Features',
+                'Blog',
+                'Shop'
+            ]
+        },
+        methods: {
+            
+        }
+    }
+);
